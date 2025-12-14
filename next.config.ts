3818179f142df0e31adc00af317993f999669b9c
NX <38133441+1NXXXN1@users.noreply.github.com>
2025-12-14 17:38:56 +0500

    Refactor next.config.ts for improved configuration

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,80 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['swiper'],
+  transpilePackages: ["swiper"],
+
   images: {
-    remotePatterns: [
-      new URL('https://image.openmoviedb.com/**'),
-      new URL('https://kinopoisk-ru.clstorage.net/**'),
+    domains: [
+      "st.kp.yandex.net",
+      "avatars.mds.yandex.net",
+      "image.tmdb.org",
+      "kinopoiskapiunofficial.tech",
+      "kinopoisk-ru.clstorage.net",
+      "image.openmoviedb.com"
     ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.yandex.net"
+      },
+      {
+        protocol: "https",
+        hostname: "**.kp.yandex.net"
+      },
+      {
+        protocol: "https",
+        hostname: "**.tmdb.org"
+      },
+      {
+        protocol: "https",
+        hostname: "**.kinopoiskapiunofficial.tech"
+      },
+      {
+        protocol: "https",
+        hostname: "**.clstorage.net"
+      },
+      {
+        protocol: "https",
+        hostname: "**.openmoviedb.com"
+      }
+    ],
+    formats: ["image/webp", "image/avif"]
   },
+
   webpack(config) {
     const fileLoaderRule = config.module.rules.find(
-      (rule: { test: { test: (arg0: string) => any } }) =>
-        rule.test?.test?.('.svg')
+      (rule: { test?: { test?: (arg0: string) => boolean } }) =>
+        rule.test?.test?.(".svg")
     );
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
+    if (fileLoaderRule) {
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...(fileLoaderRule.resourceQuery?.not || []), /url/] },
+          use: ["@svgr/webpack"]
+        }
+      );
 
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
     return config;
   },
+
   env: {
-    NEXT_KINOPOISK_KEY: process.env.NEXT_KINOPOISK_KEY,
+    NEXT_KINOPOISK_KEY: process.env.NEXT_KINOPOISK_KEY
   },
+
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone"
 };
 
 export default nextConfig;
